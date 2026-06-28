bfdfbe4b60da5c7b0630c17680ecd9e0e5b0cd58
Panupan <Ball085573@hotmail.com>
2026-06-28 17:31:01 +0700

    fix docker image

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["fish-crm.saveurnote.com"],
 };
 
 export default nextConfig;
